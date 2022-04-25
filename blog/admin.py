95ee1983c75d73bc475b217856f5bf56b0837b04
Dunas-Lopiin <plinio.figueiredo.s@gmail.com>
2022-04-25 09:12:12 -0300

    managers e querysets

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Post
-
-admin.site.register(Post)
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('created', 'author', 'published', 'status')
+    date_hierarchy = 'published'
+    raw_id_fields = ('author',)
+    list_display = ('title', 'author', 'published', 'status')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+
