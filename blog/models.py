18efb3c18af7cde75398741a10d5dd9ab4f916d4
Dunas-Lopiin <plinio.figueiredo.s@gmail.com>
2022-05-03 08:29:11 -0300

    adicionada a função de deletar postagens no blog

--- blog/models.py
+++ blog/models.py
@@ -35,6 +35,12 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail', args=[self.slug])
 
+    def get_absolute_url_update(self):
+        return reverse('post_edit', args=[self.slug])
+
+    def get_absolute_url_delete(self):
+        return reverse('post_delete', args=[self.slug])
+
     class Meta:
         ordering = ('published',)
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+# from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 # Create your views here.
@@ -13,4 +14,22 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-    context_object_name = 'custom'
+    # context_object_name = 'custom'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = ['author', 'title', 'content', 'status']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ['title', 'content', 'status']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('home')
